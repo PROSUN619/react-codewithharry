b948b238af53e191e263baa993c3bdf0f6c2e636
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-08-26 09:36:04 +0530

    video 6 : prop and component

---
+++ src/module1.mjs
@@ -0,0 +1,8 @@
+import ab, {c,d} from './module2.mjs'
+
+
+console.log(ab);
+console.log(c);
+console.log(d);
+
+
---
+++ src/module2.mjs
@@ -0,0 +1,8 @@
+
+const c = "Prasun";
+const d = "Pipa";
+const e = "Harry";
+
+export default e;
+export {c};
+export {d};
