81e753c6bd99046fe3031986bbf8d9e702349b42
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-08-27 14:18:05 +0530

    video 12: Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./Components/About";
+// import About from "./Components/About";
 import Nabvar from "./Components/Nabvar";
 import TextForm from "./Components/TextForm";
 
 function App() {
+
+ const [mode,setToogle] = useState("light");
+
+
+ const handleToogle = () =>{
+  const newMode = (mode === "light") ? "dark" : "light"
+
+  if (newMode === "dark")
+  {
+    document.body.style.backgroundColor = '#222e3f';
+    document.body.style.color = 'white';
+  }
+  else{
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = '#222e3f';
+  }
+
+  setToogle(newMode);
+ // console.log('after' + mode);
+ }
+
   return (
     <>
-      <Nabvar title="My Title" about="My About" />
-      {/* <TextForm label="Enter Text to Uppercase" /> */}
-      <About/>
+      <Nabvar title="My Title" about="My About" toogleMode={handleToogle} mode={mode}/>
+      <TextForm label="Enter Text to Uppercase" mode={mode} />
+      {/* <About/> */}
     </>
   );
 }
--- src/Components/Nabvar.js
+++ src/Components/Nabvar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export default function Nabvar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -32,7 +32,7 @@ export default function Nabvar(props) {
               </a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
@@ -42,7 +42,19 @@ export default function Nabvar(props) {
             <button className="btn btn-outline-success" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
+          <div className={`form-check form-switch text-${(props.mode === "light") ? "dark" : "light"}`}>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toogleMode}
+            />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+              Dark Mode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
@@ -51,10 +63,10 @@ export default function Nabvar(props) {
 
 Nabvar.prototype = {
   title: PropTypes.string,
-  about: PropTypes.string
+  about: PropTypes.string,
 };
 
 Nabvar.defaultProps = {
-    title: "Set Title",
-    about: "Set About"
+  title: "Set Title",
+  about: "Set About",
 };
